4515afba0bf7267f8854ab0cc234461471f68c04
entrolEC <euichan1108@gmail.com>
2024-04-24 19:28:05 +0900

    test: 버튼 테스트코드 수정

--- cypress/component/button.cy.tsx
+++ cypress/component/button.cy.tsx
@@ -1,40 +1,42 @@
-// boxButton.cy.js
 import React from 'react'
-import BoxButton from '@/components/ui/box-button'
 import { mount } from 'cypress/react18'
+import BoxButton from '@/components/ui/box-button'
+import '@/app/globals.css'
 
-describe('BoxButton Component', () => {
-  it('renders correctly with large size', () => {
-    mount(<BoxButton size='l' buttonType='rect' text='Click me!' />)
-    cy.get('button').should('have.class', 'py-[16px]')
-    cy.get('button').should('have.class', 'px-[24px]')
-    cy.get('button').should('have.class', 'rounded-[16px]')
-    cy.get('button').should('have.class', 'bg-purple-500')
-    cy.get('button').contains('Click me!')
+describe('BoxButton', () => {
+  it('renders correctly with different sizes', () => {
+    const sizes = ['l', 'm', 's'] as const
+    sizes.forEach(size => {
+      mount(<BoxButton buttonType='rect' size={size} text='Click Me' theme='default' />)
+      cy.get('button')
+        .should('have.class', `py-[${size === 'l' ? '16px' : size === 'm' ? '14px' : '8px'}]`)
+        .and('have.class', `px-[24px]`)
+        .and('have.text', 'Click Me')
+      cy.get('[data-testid="plus-icon"]').should('be.visible')
+    })
   })
 
-  it('renders correctly with medium size', () => {
-    mount(<BoxButton size='m' buttonType='rect' text='Press here' />)
-    cy.get('button').should('have.class', 'py-[14px]')
-    cy.get('button').should('have.class', 'px-[24px]')
-    cy.get('button').should('have.class', 'rounded-[8px]')
-    cy.get('button').should('have.class', 'bg-purple-500')
-    cy.get('button').contains('Press here')
+  it('applies the correct theme classes', () => {
+    const themes = ['default', 'dark', 'light'] as const
+    themes.forEach(theme => {
+      mount(<BoxButton buttonType='rect' size='m' text='Click Me' theme={theme} />)
+      cy.get('button')
+        .should('have.class', `bg-${theme === 'default' ? 'purple-500' : theme === 'dark' ? 'gray-900' : 'gary-100'}`)
+        .and('have.class', theme === 'light' ? 'text-black' : 'text-white')
+    })
   })
 
-  it('renders correctly with small size', () => {
-    mount(<BoxButton size='s' buttonType='round' text='Submit' />)
-    cy.get('button').should('have.class', 'py-[8px]')
-    cy.get('button').should('have.class', 'px-[24px]')
-    cy.get('button').should('have.class', 'rounded-[8px]')
-    cy.get('button').should('have.class', 'bg-purple-500')
-    cy.get('button').contains('Submit')
+  it('shows loading state correctly', () => {
+    mount(<BoxButton buttonType='rect' size='m' text='Loading...' theme='default' loading={true} />)
+    cy.get('[data-testid="progress-circle"]').should('not.have.class', 'hidden')
+    cy.get('[data-testid="plus-icon"]').should('have.class', 'invisible')
+    cy.get('p').should('have.class', 'invisible').and('contain', 'Loading...')
   })
 
-  it('triggers an event on click', () => {
-    const onClick = cy.spy().as('clickSpy')
-    mount(<BoxButton size='l' buttonType='rect' text='Click me!' onClick={onClick} />)
+  it('handles click events', () => {
+    const onClick = cy.stub().as('onClick')
+    mount(<BoxButton buttonType='rect' size='m' text='Click Me' theme='default' onClick={onClick} />)
     cy.get('button').click()
-    cy.get('@clickSpy').should('have.been.calledOnce')
+    cy.get('@onClick').should('have.been.calledOnce')
   })
 })
