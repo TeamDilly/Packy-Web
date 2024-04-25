2e18c2f298bbeae1ecce98bc1f8cae44ac5dc073
entrolEC <euichan1108@gmail.com>
2024-04-25 17:40:53 +0900

    chore: tailwindcss 모바일 반응형 스타일 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      height: {
+        screen: '100dvh',
+        'screen-small': '100svh',
+        'screen-large': '100lvh',
+      },
       colors: {
         gray: {
           100: '#f3f3f3',
