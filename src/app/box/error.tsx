c5d19fda961bdc19e3a95f7567667932aaae0f29
entrolEC <euichan1108@gmail.com>
2024-05-09 17:59:54 +0900

    feat: 에러 페이지 ui

--- src/app/box/error.tsx
+++ src/app/box/error.tsx
@@ -1,24 +1,24 @@
-'use client' // Error components must be Client Components
+'use client'
 
 import { useEffect } from 'react'
+import BoxButton from '@/components/ui/box-button'
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error)
   }, [error])
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className='container mx-auto flex h-dvh flex-col items-center justify-center space-y-4 px-4'>
+      <div className='container mx-auto flex flex-col items-center space-y-1'>
+        <h2 className='font-h2 break-inline text-balance break-keep text-center text-gray-900'>오류가 생겼어요</h2>
+        <p className='font-b4 break-inline text-balance break-keep text-center text-gray-600'>
+          잠시 후 다시 시도해주세요.
+        </p>
+      </div>
+      <BoxButton theme='default' onClick={() => reset()} buttonType='round' size='m'>
+        다시 시도하기
+      </BoxButton>
     </div>
   )
 }
