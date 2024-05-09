0b73fea3b38a600405065f80d5670cdc03fa0e72
entrolEC <euichan1108@gmail.com>
2024-05-09 17:27:21 +0900

    feat: 선물상자 만료 화면 ui

--- src/app/box/expired.tsx
+++ src/app/box/expired.tsx
@@ -1,3 +1,24 @@
+import BoxButton from '@/components/ui/box-button'
+import Link from 'next/link'
+
 export default function BoxExpired() {
-  return <h1>만료됨</h1>
+  return (
+    <div className='container mx-auto flex h-dvh flex-col items-center justify-between px-4 pb-[2.125rem]'>
+      <div className='container mx-auto my-auto flex flex-col items-center space-y-4'>
+        <h1 className='font-h1 break-inline text-balance break-keep text-center text-gray-900'>
+          이 선물박스는 열 수 없어요
+        </h1>
+        <p className='font-b4 break-inline text-balance break-keep text-center text-gray-800'>
+          선물박스는 한 명만 열 수 있어요.
+          <br />
+          선물박스의 주인이라면 패키에게 문의해주세요.
+        </p>
+      </div>
+      <Link href='' className='w-full'>
+        <BoxButton buttonType='rect' size='l' theme='dark' className='flex w-full justify-center text-center'>
+          문의하기
+        </BoxButton>
+      </Link>
+    </div>
+  )
 }
