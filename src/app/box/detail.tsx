8855dc54a98e1225c05af25135d52c0c3b7a8958
entrolEC <euichan1108@gmail.com>
2024-05-17 18:33:25 +0900

    style: 그리드 적용 및 미디어 쿼리 적용

--- src/app/box/detail.tsx
+++ src/app/box/detail.tsx
@@ -8,30 +8,24 @@ export default function BoxDetail({ box }: { box: GiftBoxResponse }) {
   return (
     <>
       <div className='container relative -z-50 mx-auto box-border flex h-dvh flex-col bg-[#303030] px-8'>
-        <div className='container relative mx-auto flex h-full flex-col'>
-          <p className='font-b2 absolute top-8 self-start text-white'>From. {box.senderName}</p>
+        <p className='font-b2 relative mt-8 self-start text-white'>From. {box.senderName}</p>
+        <div className='container relative mx-auto mt-8 grid h-full grid-cols-2 grid-rows-3 place-items-center'>
           <PhotoCard
-            className='absolute top-[calc(100dvh*0.12)] w-[calc(100vw*0.41)] -rotate-3'
+            className='photo-card-width h-auto -rotate-3'
             src={'https://packy-bucket.s3.ap-northeast-2.amazonaws.com/admin/design/Box/Box_1%401x.png'}
           />
-          <Sticker
-            className='absolute right-0 top-[calc(100dvh*0.17)] w-[calc(100vw*0.28)] rotate-[10deg]'
-            src={box.stickers[0].imgUrl}
-          />
-          <Sticker
-            className='absolute left-0 top-[calc(100dvh*0.43)] w-[calc(100vw*0.28)] -rotate-[10deg]'
-            src={box.stickers[0].imgUrl}
-          />
+          <Sticker className='sticker-width ml-8 rotate-[10deg]' src={box.stickers[0].imgUrl} />
+          <Sticker className='sticker-width mr-8 -rotate-[10deg]' src={box.stickers[0].imgUrl} />
           <LetterCard
             letterContent={box.letterContent}
             src={box.envelope.imgUrl}
-            className='absolute right-0 top-[calc(100dvh*0.41)] h-[calc(100vw*0.46*0.833)] w-[calc(100vw*0.46)] rotate-3'
+            className='letter-card-width rotate-3'
           />
         </div>
 
         <Image
           src={box.box.boxTop}
-          className='absolute -top-[30px] right-0 self-end'
+          className='absolute -top-[30px] right-0 -z-10 self-end'
           alt='선물상자 포장지'
           width={280}
           height={130}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+'use client'
+import React, { useState } from 'react'
+
 export default function Home() {
+  const [theme, setTheme] = useState<'default' | 'light'>('default')
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-dvh flex-col items-center justify-between p-24'>
       <p className='font-h1'>hello</p>
       <p className='font-h2'>hello</p>
       <p className='font-h3'>hello</p>
@@ -10,6 +15,18 @@ export default function Home() {
       <p className='font-b4'>hello</p>
       <p className='font-b5'>hello</p>
       <p className='font-b6'>hello</p>
+      {/*<BoxButton buttonType='rect' size='l' theme={theme}>*/}
+      {/*  hello*/}
+      {/*</BoxButton>*/}
+
+      {/*<BoxButton*/}
+      {/*  buttonType='rect'*/}
+      {/*  size='l'*/}
+      {/*  theme='light'*/}
+      {/*  onClick={() => setTheme(prev => (prev === 'default' ? 'light' : 'default'))}*/}
+      {/*>*/}
+      {/*  change*/}
+      {/*</BoxButton>*/}
     </main>
   )
 }
