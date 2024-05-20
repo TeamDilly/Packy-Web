2c6b2533c1cef1ad7f89d751a70a56b5f1e20678
entrolEC <euichan1108@gmail.com>
2024-05-20 14:52:13 +0900

    fix: 크로스브라우징 letter-card 스타일 수정

--- src/components/box/letter-card.tsx
+++ src/components/box/letter-card.tsx
@@ -14,15 +14,15 @@ export default function LetterCard({
   className: string
 }) {
   return (
-    <button type='button' className={clsx(className)} onClick={onClick}>
+    <button type='button' className={clsx(className, 'relative')} onClick={onClick}>
       <div
         className={
-          'font-b6 relative h-[87.5%] w-[87.5%] overflow-hidden rounded-lg bg-white px-2.5 py-1.5 text-gray-900'
+          'font-b6 absolute inset-0 h-[87.5%] w-[87.5%] overflow-hidden rounded-lg bg-white px-2.5 py-1.5 text-gray-900'
         }
       >
         {letterContent}
       </div>
-      <div className={'right relative -top-[75%] left-[11.1%] h-[87.5%] w-[87.5%]'}>
+      <div className={'right absolute bottom-0 left-[11.1%] h-[87.5%] w-[87.5%]'}>
         <Image src={src} alt='편지봉투' width={500} height={500} />
       </div>
     </button>
