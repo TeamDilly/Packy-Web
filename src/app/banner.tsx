f61fae6db23d622e284e61ff4571dcd7e94dc64a
entrolEC <euichan1108@gmail.com>
2024-05-25 21:08:22 +0900

    feat: 다운로드

--- src/app/banner.tsx
+++ src/app/banner.tsx
@@ -1,7 +1,28 @@
+'use client'
 import Image from 'next/image'
 import BoxButton from '@/components/ui/box-button'
 
 export default function Banner() {
+  const googlePlayStoreUrl = 'https://play.google.com/store/apps/details?id=com.packy&hl=ko'
+  const appStoreUrl = 'https://apps.apple.com/kr/app/%ED%8C%A8%ED%82%A4/id6477327987'
+
+  const handleClick = () => {
+    if (typeof navigator !== 'undefined') {
+      const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera
+
+      if (/android/i.test(userAgent)) {
+        console.log('android')
+        window.location.href = googlePlayStoreUrl
+      } else if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
+        console.log('ios')
+        window.location.href = appStoreUrl
+      } else {
+        console.log('others')
+        window.location.href = appStoreUrl
+      }
+    }
+  }
+
   return (
     <div className='sticky z-40 flex justify-between bg-gray-100 px-4 py-3'>
       <div className='flex space-x-2'>
@@ -17,7 +38,7 @@ export default function Banner() {
           <p className='font-b4 text-gray-900'>앱 다운받고 선물박스 저장하기</p>
         </div>
       </div>
-      <BoxButton buttonType='rect' size='s' theme='dark' className='text-nowrap px-4'>
+      <BoxButton buttonType='rect' size='s' theme='dark' className='text-nowrap px-4' onClick={handleClick}>
         다운로드
       </BoxButton>
     </div>
