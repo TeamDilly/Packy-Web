e4107f0f9c94ac6a410698173cce8e20f59e744c
entrolEC <euichan1108@gmail.com>
2024-05-26 13:16:28 +0900

    feat: loading indicator 추가

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,9 @@
+import ProgressCircle from '@/components/icon/ProgressCircle'
+
 export default function Loading() {
-  return <p>Loading...</p>
+  return (
+    <div className='flex h-dvh w-screen animate-spin items-center justify-center'>
+      <ProgressCircle />
+    </div>
+  )
 }
