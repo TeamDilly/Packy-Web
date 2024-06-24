4233969a386a621dc6635689baace18074563253
entrolEC <euichan1108@gmail.com>
2024-06-24 15:20:09 +0900

    style: fading 자연스럽게

--- src/app/box-container.tsx
+++ src/app/box-container.tsx
@@ -2,6 +2,7 @@ import { BoxStatus, GiftBoxResponse } from '@/lib/types'
 import { useLottie } from '@/lib/fetchers'
 import dynamic from 'next/dynamic'
 import Loading from '@/app/loading'
+import OpenMotion from '@/app/open-motion'
 
 type BoxContainerProps = {
   data: GiftBoxResponse
@@ -14,14 +15,13 @@ const BoxDetail = dynamic(() => import('@/app/detail'))
 const Banner = dynamic(() => import('@/app/banner'))
 const Timer = dynamic(() => import('@/app/timer'))
 const BoxArr = dynamic(() => import('@/app/arr'))
-const OpenMotion = dynamic(() => import('@/app/open-motion'))
 
 export default function BoxContainer({ data, opened, onOpenClick, onOpenComplete }: BoxContainerProps) {
   const urlObj = new URL(data.box.boxLottie)
   const path = urlObj.pathname
   const { data: lottieData, error: lottieError, isLoading: lottieLoading } = useLottie(path)
 
-  if (data === undefined || data.box === undefined) throw new Error()
+  if (data.box === undefined) throw new Error()
   if (lottieLoading) return <Loading />
   if (lottieError || !lottieData) throw new Error('Failed to load lottie data')
 
@@ -31,7 +31,7 @@ export default function BoxContainer({ data, opened, onOpenClick, onOpenComplete
       <Banner />
       {opened === 'opened' && <BoxDetail box={data} />}
       {(opened === 'opening' || opened === 'fading') && (
-        <OpenMotion lottieData={lottieData} onComplete={onOpenComplete} />
+        <OpenMotion lottieData={lottieData} opened={opened} onComplete={onOpenComplete} />
       )}
       {(opened === 'closed' || opened === 'fading') && data && (
         <BoxArr box={data} onOpenClick={onOpenClick} opened={opened} />
--- src/app/open-motion.tsx
+++ src/app/open-motion.tsx
@@ -1,14 +1,21 @@
 import Lottie from 'lottie-react'
 import { AnimationEventHandler } from 'react'
+import { cn } from '@/lib/util'
 
 type OpenMotion = {
   lottieData: JSON
   onComplete: AnimationEventHandler
+  opened: 'opening' | 'fading'
 }
 
-export default function OpenMotion({ lottieData, onComplete }: OpenMotion) {
+export default function OpenMotion({ lottieData, onComplete, opened }: OpenMotion) {
   return (
-    <div className='container absolute flex h-[calc(100dvh-4rem)] items-center justify-center overflow-visible'>
+    <div
+      className={cn(
+        { 'item-fadein': opened === 'fading' },
+        'container absolute flex h-[calc(100dvh-4rem)] items-center justify-center overflow-visible',
+      )}
+    >
       <Lottie animationData={lottieData} loop={false} onComplete={onComplete} style={{ width: '100%' }} />
     </div>
   )
