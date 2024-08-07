42da576d981467be2d19fb335558d34e4aeda91c
entrolEC <euichan1108@gmail.com>
2024-08-08 03:10:38 +0900

    fix: 새탭에서 branch 링크 열기

--- src/app/banner.tsx
+++ src/app/banner.tsx
@@ -3,10 +3,6 @@ import Image from 'next/image'
 import BoxButton from '@/components/ui/box-button'
 
 export default function Banner({ branchUrl }: { branchUrl: string }) {
-  const handleClick = () => {
-    window.location.href = branchUrl
-  }
-
   return (
     <div className='sticky z-40 flex justify-between bg-gray-100 px-4 py-3'>
       <div className='flex space-x-2'>
@@ -23,9 +19,11 @@ export default function Banner({ branchUrl }: { branchUrl: string }) {
           <p className='font-b4 text-gray-900'>앱 다운받고 선물박스 저장하기</p>
         </div>
       </div>
-      <BoxButton buttonType='rect' size='s' theme='dark' className='text-nowrap px-4' onClick={handleClick}>
-        다운로드
-      </BoxButton>
+      <a target='_blank' href={branchUrl}>
+        <BoxButton buttonType='rect' size='s' theme='dark' className='text-nowrap px-4'>
+          다운로드
+        </BoxButton>
+      </a>
     </div>
   )
 }
