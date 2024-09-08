74a6858730e6aefb0c2ce8921f270ebec1391e8d
entrolEC <euichan1108@gmail.com>
2024-09-08 19:23:06 +0900

    feat: noticeList with dummy

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import { useNotice } from '@/lib/fetchers'
+import { useNotice, useNoticeList } from '@/lib/fetchers'
 import Image from 'next/image'
 import Loading from '@/app/loading'
 import { Suspense, useEffect, useState } from 'react'
 import { NOTICE_NOT_FOUND } from '@/lib/types'
 import { notFound } from 'next/navigation'
+import Link from 'next/link'
 
 function NoticePage({ id }: { id: string }) {
   const { data, isLoading, error } = useNotice(id)
@@ -30,6 +31,27 @@ function NoticePage({ id }: { id: string }) {
   )
 }
 
+function NoticeListPage() {
+  const { data, isLoading, error } = useNoticeList()
+  if (isLoading) return <Loading />
+  if (error) throw new Error('Failed to load notice data')
+
+  return (
+    <div className='flex h-screen flex-col items-center gap-3 overflow-auto bg-gray-100 p-4'>
+      {data?.data &&
+        data.data.map(({ imgUrl, noticeUrl }, idx) => (
+          <Link
+            key={imgUrl}
+            href={noticeUrl}
+            className='overflow-hidden rounded-[1.25rem] transition ease-in-out hover:scale-90'
+          >
+            <Image src={imgUrl} layout='responsive' width={480} height={84} priority alt={`공지사항 이미지 ${idx}`} />
+          </Link>
+        ))}
+    </div>
+  )
+}
+
 export default function Page() {
   const [params, setParams] = useState<Record<string, string>>({})
 
@@ -43,6 +65,5 @@ export default function Page() {
   }, [])
 
   const id: string | undefined = params['id']
-
-  return <Suspense fallback={<Loading />}>{id && <NoticePage id={id} />}</Suspense>
+  return <Suspense fallback={<Loading />}>{id ? <NoticePage id={id} /> : <NoticeListPage />}</Suspense>
 }
--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -70,3 +70,46 @@ export function useNotice(id: string) {
     error: error,
   }
 }
+
+export function useNoticeList() {
+  // const { data, error, isLoading } = useSWR<NoticeResponse, Error>(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/admin/notices`,
+  //   fetcher,
+  // )
+  const { data, error, isLoading } = useSWR<WebNoticeResponse, Error>(
+    `${process.env.NEXT_PUBLIC_API_URL}/admin/notices/web/${1}`,
+    fetcher,
+  )
+
+  const dummy = {
+    data: [
+      {
+        imgUrl:
+          'https://packy-bucket.s3.ap-northeast-2.amazonaws.com/admin/notice/%ED%8C%A8%ED%82%A4%EC%86%8C%EA%B0%9C/%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5%402.png',
+        noticeUrl: 'https://www.naver.com/',
+      },
+
+      {
+        imgUrl:
+          'https://packy-bucket.s3.ap-northeast-2.amazonaws.com/admin/notice/%ED%8C%A8%ED%82%A4%EC%86%8C%EA%B0%9C/%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5%402.png',
+        noticeUrl: 'https://www.naver.com/',
+      },
+      {
+        imgUrl:
+          'https://packy-bucket.s3.ap-northeast-2.amazonaws.com/admin/notice/%ED%8C%A8%ED%82%A4%EC%86%8C%EA%B0%9C/%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5%402.png',
+        noticeUrl: 'https://www.naver.com/',
+      },
+      {
+        imgUrl:
+          'https://packy-bucket.s3.ap-northeast-2.amazonaws.com/admin/notice/%ED%8C%A8%ED%82%A4%EC%86%8C%EA%B0%9C/%E1%84%87%E1%85%A2%E1%84%82%E1%85%A5%402.png',
+        noticeUrl: 'https://www.naver.com/',
+      },
+    ],
+  }
+
+  return {
+    data: dummy,
+    isLoading,
+    error: error,
+  }
+}
